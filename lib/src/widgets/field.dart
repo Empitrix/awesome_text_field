a6c33c125de6bb6197d55d25b33c1c82e48005f8
Empitrix <empitrix@gmail.com>
2024-01-21 16:49:09 +0100

    Added: onChanged, tabSize, decoration{no-input} && CLEAN-UP

--- lib/src/widgets/field.dart
+++ lib/src/widgets/field.dart
@@ -5,140 +5,23 @@ import 'package:awesome_text_field/src/utils/keyboard.dart';
 import 'package:awesome_text_field/src/utils/paragraph_data.dart';
 import 'package:awesome_text_field/src/widgets/vertical_scrollable.dart';
 
-/*
-
-class AwesomeTextField extends StatelessWidget {
-	final TextEditingController controller;
-	final TextStyle style;
-	final FocusNode? focusNode;
-	const AwesomeTextField({
-		super.key,
-		required this.controller,
-		required this.style,
-		this.focusNode
-	});
-
-
-	@override
-	Widget build(BuildContext context) {
-
-		FocusNode keyboardFocus = focusNode ?? FocusNode();
-		ValueNotifier<LineStatus> lineStatus = ValueNotifier(LineStatus(lineNumber: 0, lineHeight: 0, currentLine: 0, longestLine: 1));
-		
-		if(controller.text.isNotEmpty){
-			controller.text = controller.text.replaceAll("\u000a", "\u000b");
-		}
-
-		controller.addListener(() {
-			updateDisplayedLineCount(
-				context: context,
-				controller: controller,
-				style: style,
-				onUpdate: (LineStatus status){
-					lineStatus.value = status;
-				}
-			);
-		});
-
-		return Builder(
-			builder: (BuildContext context){
-				Widget theWidget = IntrinsicHeight(
-					child: Row(
-						mainAxisAlignment: MainAxisAlignment.start,
-						crossAxisAlignment: CrossAxisAlignment.start,
-						children: [
-							ValueListenableBuilder(
-								valueListenable: lineStatus,
-								builder: (_, value, __){
-									return Container(
-										// margin: EdgeInsets.only(top: isDesktop() ? 10 : 11.5),
-										decoration: BoxDecoration(
-											color: Theme.of(context).colorScheme.secondaryContainer,
-											border: const Border(right: BorderSide(color: Colors.amber, width: 1))
-										),
-										width: 25,
-										height: MediaQuery.of(context).size.height + ((value.lineNumber - 10) * value.lineHeight),
-										child: Column(
-											children: [
-												SizedBox(height: isDesktop() ? 10 : 11.5),
-												for(int l = 0; l < (value.lineNumber); l++) Container(
-													color: (l + 1 == value.currentLine) ? Colors.amber : Theme.of(context).colorScheme.secondaryContainer,
-													height: value.lineHeight,
-													child: Center(
-														child: FittedBox(child: Text(
-															"${l+1} ",
-															style: TextStyle(
-																color: (l + 1 == value.currentLine) ? Colors.black : Colors.amber,
-																fontFamily: "RobotoMono",
-																fontWeight: FontWeight.bold
-															)
-														))
-													),
-												)
-											],
-										),
-									);
-								},
-							),
-							Expanded(
-								child: ValueListenableBuilder(
-									valueListenable: lineStatus,
-									builder: (_, value, __){
-										return VerticalScrollable(
-											width: MediaQuery.of(context).size.width + value.longestLine,
-											child: IntrinsicHeight(
-												child: Container(
-													margin: EdgeInsets.only(
-														top: isDesktop() ?  2 : 0,
-														left: 5
-													),
-													child: KeyboardListener(
-														focusNode: FocusNode(),
-														autofocus: true,
-														onKeyEvent: (KeyEvent event) async {
-															TextEditingValue? ctrlValue = applyKeyboardAction(
-																event: event, controller: controller, tabSize: 4);
-															if(ctrlValue != null){ controller.value = ctrlValue; }
-															keyboardFocus.requestFocus();
-														},
-														child: TextField(
-															controller: controller,
-															focusNode: keyboardFocus,
-															expands: true,
-															keyboardType: TextInputType.multiline,
-															cursorHeight: value.lineHeight,
-															style: style,
-															decoration: const InputDecoration(border: InputBorder.none, hintText: "Write..."),
-															autofocus: true,
-															maxLines: null,
-														)
-													)
-												),
-											)
-										);
-									},
-								),
-							)
-						],
-					),
-				);
-				return theWidget;
-			},
-		);
-	}
-}
-*/
-
 
 class AwesomeTextField extends StatefulWidget {
 	final TextEditingController controller;
 	final TextStyle style;
 	final FocusNode? focusNode;
+	final InputDecoration decoration;
+	final ValueChanged<String>? onChanged;
+	final int tabSize;
 	const AwesomeTextField({
 		super.key,
 		required this.controller,
 		required this.style,
-		this.focusNode
+		this.focusNode,
+		this.decoration = const InputDecoration(border: InputBorder.none, hintText: ""),
+		this.onChanged,
+		this.tabSize = 4,
+
 });
 
 	@override
@@ -224,7 +107,8 @@ class _AwesomeTextFieldState extends State<AwesomeTextField> {
 										// width: value.lineNumber > 0 ? 25 : 0,
 										// duration: const Duration(seconds: 1),
 										width: 25,
-										height: MediaQuery.of(context).size.height + ((value.lineNumber - 10) * value.lineHeight),
+										height: MediaQuery.of(context).size.height +
+											((value.lineNumber - 10) * value.lineHeight),
 										child: Column(
 											children: [
 												SizedBox(height: topBufferMargin),
@@ -277,9 +161,10 @@ class _AwesomeTextFieldState extends State<AwesomeTextField> {
 															keyboardType: TextInputType.multiline,
 															cursorHeight: lHeight,
 															style: widget.style,
-															decoration: const InputDecoration(border: InputBorder.none, hintText: "Write..."),
+															decoration: widget.decoration.copyWith(border: InputBorder.none),
 															autofocus: true,
 															maxLines: null,
+															onChanged: widget.onChanged,
 														)
 													)
 												),
